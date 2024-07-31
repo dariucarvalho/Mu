ed4a5d3b4620af43c9f2da16cbe3a965c131e425
Dariu Carvalho <149876554+dariucarvalho@users.noreply.github.com>
2024-07-31 00:05:51 -0300

    Inicio

---
+++ main.js
@@ -0,0 +1,6 @@
+function login() {
+  window.location.href = "./usuario.html"; // Corrigido para redirecionar corretamente
+}
+function sair() {
+  window.location.href = "./index.html"; // Corrigido para redirecionar corretamente
+}
